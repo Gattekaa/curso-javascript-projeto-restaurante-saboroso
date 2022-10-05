d91e1f04a92e5a5b27b678a886024cfe9eae1f22
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-10-05 13:29:16 -0300

    Refactoring the admin page menu code, now the menus are rendered by a forEach

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,38 @@
 var express = require('express');
-const users = require('./../inc/users');
-var user = require('./../inc/users');
+var users = require('./../inc/users');
+var admin = require('./../inc/admin')
 var router = express.Router();
 
+router.use(function(req, res, next){
+
+    if(['/login'].indexOf(req.url) === -1 && !req.session.user) {
+        res.redirect("/admin/login");
+    } else {
+        next();
+    }
+
+});
+
+router.use(function(req, res, next) {
+
+    req.menus = admin.getMenus();
+
+    next();
+
+})
+
+router.get("/logout", function(req, res, next){
+
+    delete req.session.user 
+    res.redirect("/admin/login")
+
+})
 
 router.get("/", function(req, res, next){
 
-    res.render("admin/index");
+    res.render("admin/index", {
+        menus: req.menus
+    });
 
 });
 
@@ -17,7 +43,7 @@ router.post("/login", function(req, res, next){
     } else if (!req.body.password) {
         users.render(req, res, "Preencha o campo senha.")
     } else {
-        user.login(req.body.email, req.body.password).then(user => {
+        users.login(req.body.email, req.body.password).then(user => {
 
             req.session.user = user; 
 
@@ -41,34 +67,43 @@ router.get("/login", function(req, res, next){
 
 router.get("/contacts", function(req, res, next){
 
-    res.render("admin/contacts");
+    res.render("admin/contacts", {
+        menus: req.menus
+    });
 
 });
 
 
 router.get("/emails", function(req, res, next){
 
-    res.render("admin/emails");
+    res.render("admin/emails", {
+        menus: req.menus
+    });
 
 });
 
 router.get("/menus", function(req, res, next){
 
-    res.render("admin/menus");
+    res.render("admin/menus", {
+        menus: req.menus
+    });
 
 });
 
 router.get("/reservations", function(req, res, next){
 
     res.render("admin/reservations", {
-        date:{}
+        date:{},
+        menus: req.menus
     });
 
 });
 
 router.get("/users", function(req, res, next){
 
-    res.render("admin/users");
+    res.render("admin/users", {
+        menus: req.menus
+    });
 
 });
 
