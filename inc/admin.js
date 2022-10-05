f204e2c5eb58c8d8bd488742269c8b98be4145c8
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-10-05 13:54:56 -0300

    Receiving number of tables through the database and showing it to the user

--- inc/admin.js
+++ inc/admin.js
@@ -1,6 +1,30 @@
+var conn = require('./db')
+
 module.exports = {
 
-    getParams(req, params){
+    dashboard() {
+        return new Promise((resolve, reject) => {
+
+            conn.query(`
+            SELECT
+                (SELECT COUNT(*) FROM tb_contacts) AS nrcontacts,
+                (SELECT COUNT(*) FROM tb_menus) AS nrmenus,
+                (SELECT COUNT(*) FROM tb_reservations) AS nrreservations,
+                (SELECT COUNT(*) FROM tb_users) AS nrusers;
+            `, (err, results)=>{
+
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results[0]);
+                }
+
+            });
+
+        });
+    },
+
+    getParams(req, params) {
 
         return Object.assign({}, {
             menus: req.menus,
@@ -50,9 +74,9 @@ module.exports = {
             }
         ];
 
-        menus.map(menu=>{
+        menus.map(menu => {
 
-            if(menu.href === `/admin${req.url}`) menu.active = true;
+            if (menu.href === `/admin${req.url}`) menu.active = true;
 
             console.log(req.url, menu.href)
 
