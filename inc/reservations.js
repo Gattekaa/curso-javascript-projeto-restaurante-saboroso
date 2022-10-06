d76b503080ee6dada8e66f42dee96056ad1d9530
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-10-06 13:41:14 -0300

    The administrative page of reservations is now dynamic, the data is obtained through the database.

--- inc/reservations.js
+++ inc/reservations.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 var conn = require("./db")
 
 module.exports = {
@@ -17,19 +18,47 @@ module.exports = {
 
         return new Promise((resolve, reject)=> {
 
-            let date = fields.date.split('/');
-            fields.date = `${date[2]}-${date[1]}-${date[0]}`;
+            if (fields.date.indexOf('/') > -1) {
 
-            conn.query(`
-            INSERT INTO tb_reservations (name, email, people, date, time)
-            VALUES(?, ?, ?, ?, ?)
-        `, [
-            fields.name,
-            fields.email,
-            fields.people,
-            fields.date,
-            fields.time
-        ], (err, results)=> {
+                let date = fields.date.split('/');
+                fields.date = `${date[2]}-${date[1]}-${date[0]}`;
+    
+            }
+
+            
+            let query, params = [
+                fields.name,
+                fields.email,
+                fields.people,
+                fields.date,
+                fields.time
+            ];
+
+            if (parseInt(fields.id) > 0) {
+
+                query = `
+                    UPDATE tb_reservations
+                    SET
+                        name = ?,
+                        email = ?,
+                        people = ?,
+                        date = ?,
+                        time = ?
+                    WHERE id = ?
+                `;
+
+                params.push(fields.id);
+
+            } else {
+
+                query = `
+                INSERT INTO tb_reservations (name, email, people, date, time)
+                VALUES(?, ?, ?, ?, ?)
+            `
+
+            }
+
+            conn.query(query, params, (err, results)=> {
 
             if (err) {
                 reject(err)
@@ -41,5 +70,23 @@ module.exports = {
 
         })
 
-    }
+    },
+    getReservations() {
+
+        return new Promise((resolve, reject) => {
+
+            conn.query(`
+                    SELECT * FROM tb_reservations ORDER BY date DESC
+                `, (err, results) => {
+                if (err) {
+                    reject
+                }
+
+                resolve(results);
+
+            });
+
+        });
+
+    },
 }
